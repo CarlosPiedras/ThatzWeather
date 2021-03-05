a63deef5f8c25d3c107c661f89d2e3c71c1540fc
CarlosPiedras <capouca2000@gmail.com>
2021-03-05 02:39:18 +0100

    Primera subida

---
+++ app/Http/Controllers/TemperaturasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Temperaturas;
+use Illuminate\Http\Request;
+
+class TemperaturasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Temperaturas  $temperaturas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Temperaturas $temperaturas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Temperaturas  $temperaturas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Temperaturas $temperaturas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Temperaturas  $temperaturas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Temperaturas $temperaturas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Temperaturas  $temperaturas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Temperaturas $temperaturas)
+    {
+        //
+    }
+}
